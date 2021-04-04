f1ab95b3beb6bcf1b13ac0dd0c147e6a7993b6e2
Morgado <morgado@Corentins-MacBook-Pro.local>
2021-04-04 21:04:13 +0200

    UPDATE - Day6 part2 dirty version but working

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,8 +2,6 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 
-type HashIter = Box<dyn Iterator<Item = (char, i32)>>;
-
 fn main() {
     let input_answers = include_str!("day6")
         .lines()
@@ -13,8 +11,6 @@ fn main() {
         .map(|(_, identity)| {
             identity
                 .map(str::chars)
-                .map(|chars| chars.map(|c| c).collect::<Vec<char>>())
-                .into_iter()
                 .map(|v| {
                     v.into_iter().fold(HashMap::new(), |mut acc, values| {
                         let count = acc.entry(values).or_insert_with(|| 0);
@@ -26,44 +22,45 @@ fn main() {
         })
         .collect::<Vec<Vec<HashMap<char, i32>>>>();
 
-    // dbg!(&input_answers);
-
-    // let resolve1 = &input_answers
-    //     .into_iter()
-    //     .map(|v| {
-    //         v.into_iter()
-    //             .fold(Box::new(std::iter::empty()) as HashIter, |acc, hm| {
-    //                 Box::new(acc.chain(Box::new(hm.into_iter()) as HashIter)) as HashIter
-    //             })
-    //             .fold(HashMap::new(), |mut acc, tup| {
-    //                 let count = acc.entry(tup.0).or_insert_with(|| 0);
-    //                 *count += 1;
-    //                 acc
-    //             })
-    //     })
-    //     .collect::<Vec<_>>()
-    //     .iter()
-    //     .map(|v| v.keys().len())
-    //     .collect::<Vec<_>>()
-    //     .iter()
-    //     .sum::<usize>();
-    // dbg!(&resolve1);
-
-    let resolve2 = &input_answers
+    let resolve = &input_answers
         .into_iter()
         .map(|v| {
-            let mut final_hm: HashMap<char, i32> = HashMap::new();
-            v.into_iter().for_each(|hm| {
-                final_hm.extend(hm.into_iter().map(|(k, v)| {
-                    // let count = final_hm.entry(k).or_insert_with(|| 0);
-                    // *count += 1;
-
-                    (k.clone(), v.clone())
-                }))
-            });
-            final_hm
+            let taille = &v.len();
+            v.into_iter().fold(HashMap::new(), |mut acc, hm| {
+                hm.into_iter().for_each(|(k, _)| {
+                    let count = acc.entry(k).or_insert_with(|| *taille);
+                    *count -= &1;
+                });
+                acc
+            })
         })
         .collect::<Vec<_>>();
 
+    dbg!(&resolve);
+
+    let resolve1 = &resolve
+        .iter()
+        .map(|v| v.keys().len())
+        .collect::<Vec<_>>()
+        .iter()
+        .sum::<usize>();
+
+    let resolve2 = &resolve
+        .iter()
+        .map(|hm| {
+            hm.into_iter().fold(Vec::new(), |mut acc: Vec<_>, values| {
+                if values.1 == &0 {
+                    acc.push(*values.0)
+                }
+                acc
+            })
+        })
+        .collect::<Vec<_>>()
+        .into_iter()
+        .map(|v| v.len())
+        .collect::<Vec<_>>()
+        .iter()
+        .sum::<usize>();
+    dbg!(&resolve1);
     dbg!(&resolve2);
 }
